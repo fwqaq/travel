4c9187a94a5ff250ab6cdf468687981252340bb2
fwqaq <45515058+fwqaq@users.noreply.github.com>
2020-03-29 17:10:47 +0800

    ''

--- src/pages/winshop/components/Detail.vue
+++ src/pages/winshop/components/Detail.vue
@@ -1,49 +1,49 @@
 <!-- 容器组件 -->
 <template>
 	<div class="header">
-      <table>
+      <table class="header-table">
         <tr>
           <td>
             <div class="iconfont back-icon">&#xe604; </div>
-            <div>无接触房</div>
+            <div class="back-font">无接触房</div>
           </td>
           <td>
             <div class="iconfont back-icon">&#xe61d; </div>
-            <div>周租月租</div>
+            <div class="back-font">周租月租</div>
           </td>
           <td>
             <div class="iconfont back-icon">&#xe622;</div>
-            <div>全家出游</div>
+            <div class="back-font">全家出游</div>
           </td>
           <td>
             <div class="iconfont back-icon">&#xe620;</div>
-            <div>四合院</div>
+            <div class="back-font">四合院</div>
           </td>
           <td>
             <div class="iconfont back-icon">&#xe650;</div>
-            <div>商家自营</div>
+            <div class="back-font">商家自营</div>
           </td>
         </tr>
         <tr>
           <td>
             <div class="iconfont back-icon">&#xe614;</div>
-            <div>青年旅社</div>
+            <div class="back-font">青年旅社</div>
           </td>
           <td>
             <div class="iconfont back-icon">&#xe6ff;</div>
-            <div>新房特惠</div>
+            <div class="back-font">新房特惠</div>
           </td>
           <td>
             <div class="iconfont back-icon">&#xe63e;</div>
-            <div>人气民宿</div>
+            <div class="back-font">人气民宿</div>
           </td>
           <td>
             <div class="iconfont back-icon">&#xe684;</div>
-            <div>住房攻略</div>
+            <div class="back-font">住房攻略</div>
           </td>
           <td>
             <div class="iconfont back-icon">&#xe602;</div>
-            <div>美拍屋</div>
+            <div class="back-font">美拍屋</div>
           </td>
           
         </tr>
@@ -62,5 +62,19 @@ export default {
 <style lang="stylus" scoped>
   .header
     position: absolute
-    top: 9.5rem
+    top: 9.4rem
+    width: 100%
+    height: 2rem
+    background: #ddd
+    .header-table
+      width: 100%
+      height: 100%
+      border:solid 1px #F0F8FF
+      margin: auto
+      .back-icon
+        font-size: .6rem
+        text-align: center
+      .back-font
+        font-size: .3rem
+        text-align: center
 </style>
